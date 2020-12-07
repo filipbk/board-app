37569a2f68e8b6da008de59788e091c0f475ba36
Filip Bogatko <236634@student.pwr.edu.pl>
2020-12-07 17:57:11 +0200

    Option to upload photo in offer form

--- frontend/src/Offer/OfferForm/OfferForm.js
+++ frontend/src/Offer/OfferForm/OfferForm.js
@@ -1,6 +1,6 @@
-import React from "react";
-import {Button, Form, Input} from "antd";
-import './OfferForm.css'
+import React from 'react';
+import {Button, Col, Form, Input, Row, Upload} from 'antd';
+import './OfferForm.css';
 
 export class OfferForm extends React.Component {
   constructor(props) {
@@ -11,53 +11,110 @@ export class OfferForm extends React.Component {
       content: undefined,
       phoneNumber: undefined,
       price: undefined,
-      editMode: false
-    }
+      editMode: false,
+      fileList: []
+    };
   }
-  render() {
-    return (
-      <div className='form-wrapper'>
-        <Form className=''>
-          <Form.Item
-            name='title'
-            rules={[
-              {
-                required: true,
-                message: 'Please input offer title!'
-              }
-            ]}
-          >
-            <Input
-              id='title'
-              placeholder='Title'
-            />
-          </Form.Item>
-          <Form.Item
-            name='phoneNumber'
-            rules={[
-              {
-                required: true,
-                message: 'Please input your phone number!'
-              }
-            ]}
-          >
-            <Input
-              id='phoneNumber'
-              placeholder='Phone number'
-            />
-          </Form.Item>
 
-          <Form.Item className='btn-wrapper'>
-            <Button
-              type='primary'
-              htmlType='submit'
-              className='form-button'
+  async onPreview(file) {
+    let src = file.url;
+
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow.document.write(image.outerHTML);
+  }
+
+  onChange({fileList: newFileList}) {
+    this.setState({fileList: newFileList});
+  }
+
+  render() {
+    const {fileList} = this.state;
+    const layout = {
+      labelCol: {span: 6},
+      wrapperCol: {span: 18}
+    };
+
+    return (
+      <Row>
+        <Col span={20} offset={2}>
+          <Form
+            {...layout}
+            className=''
+            onFinish={() => console.log(this.state)}
+          >
+            <Form.Item
+              name='title'
+              label='Title'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input offer title!'
+                }
+              ]}
             >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+              <Input
+                id='title'
+                placeholder='Title'
+                onChange={(e) => this.setState({title: e.target.value})}
+              />
+            </Form.Item>
+            <Form.Item
+              name='phoneNumber'
+              label='Phone number'
+              rules={[
+                {
+                  pattern: /\d{9}/,
+                  required: true,
+                  message: 'Please input your phone number!'
+                }
+              ]}
+            >
+              <Input id='phoneNumber' placeholder='Phone number' />
+            </Form.Item>
+            <Form.Item
+              name='price'
+              label='Price'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input price!',
+                  type: 'number'
+                }
+              ]}
+            >
+              <Input id='price' placeholder='Price' />
+            </Form.Item>
+
+            <Upload
+              beforeUpload={() => false}
+              listType='picture-card'
+              fileList={fileList}
+              onPreview={(e) => this.onPreview(e)}
+              onChange={(e) => this.onChange(e)}
+            >
+              {fileList.length < 5 && '+ Upload'}
+            </Upload>
+
+            <Form.Item
+              className='btn-wrapper'
+              wrapperCol={{...layout.wrapperCol, offset: 8}}
+            >
+              <Button type='primary' htmlType='submit' className='form-button'>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
