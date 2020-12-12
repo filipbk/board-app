e20de39a5216d890ac2b7c2607f1a7e4b067f771
FilipMatuszczak <chips1331@gmail.com>
2020-12-12 18:52:52 +0200

    Add older version

--- backend/src/offer/offer.controller.ts
+++ backend/src/offer/offer.controller.ts
@@ -1,13 +1,13 @@
 import {OfferService} from "./offer.service";
 import {OfferCreateDto} from "./offer.create.dto";
-import {Body, Controller, Post, ValidationPipe} from "@nestjs/common";
+import {Body, Controller, Post} from "@nestjs/common";
 
 @Controller('offer')
 export class OfferController {
     constructor(private service: OfferService) {}
 
     @Post()
-    async postUser(@Body(new ValidationPipe({transform: true})) offer: OfferCreateDto) {
+    async postUser(@Body() offer: OfferCreateDto) {
         console.log(offer);
         return this.service.insertOffer(offer);
     }
