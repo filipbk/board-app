137feeac64b09ef4aee21e6cf8d58694ff051677
FilipMatuszczak <chips1331@gmail.com>
2020-12-13 22:54:13 +0200

    Test fix

--- backend/src/offer/offer.controller.spec.ts
+++ backend/src/offer/offer.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OfferController } from './offer.controller';
+import {OfferService} from "./offer.service";
+import {OfferRepository} from "./offer.repository";
+import {CategoryRepository} from "../category/category.repository";
 
 describe('Offer Controller', () => {
   let controller: OfferController;
@@ -7,6 +10,7 @@ describe('Offer Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OfferController],
+      providers: [OfferService, OfferRepository, CategoryRepository],
     }).compile();
 
     controller = module.get<OfferController>(OfferController);
