81be653483b3a8e9bbaffca8a19ae639c19828ad
Patryk Kramarczyk <pkramarczyk16@gmail.com>
2020-12-14 14:12:09 +0200

    Fix to tests

--- frontend/src/Offers/OfferItem.test.js
+++ frontend/src/Offers/OfferItem.test.js
@@ -7,14 +7,14 @@ describe('OfferItem', () => {
     const offer = {
       id: 1,
       title: 'Lotlux',
-      thumbnail: 'http://dummyimage.com/400x404.jpg/cc0000/ffffff',
-      price: 69329.27
+      image: 'http://dummyimage.com/400x404.jpg/cc0000/ffffff',
+      money: 69329.27
     };
 
     const offerItem = mount(<OfferItem {...offer} />);
     expect(offerItem).toBeInTheDocument;
-    expect(offerItem.find('#offer-1-thumbnail').at(0).prop('src')).toEqual(offer.thumbnail);
+    expect(offerItem.find('#offer-1-thumbnail').at(0).prop('src')).toEqual(offer.image);
     expect(offerItem.find('#offer-1-title').at(0).text()).toEqual(offer.title);
-    expect(offerItem.find('#offer-1-price').at(0).text()).toEqual(`${offer.price} zł`);
+    expect(offerItem.find('#offer-1-price').at(0).text()).toEqual(`${offer.money} zł`);
   });
 });
